119f8d669e0ab14d76f9459917cde5d075777a7d
Ariwa042 <nwezemichael45@gmail.com>
2024-08-29 02:16:04 +0100

    commit .

--- Pidrop/settings.py
+++ Pidrop/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -53,7 +52,6 @@ MIDDLEWARE = [
 ]
 
 # WhiteNoise settings
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'Pidrop.urls'
 
